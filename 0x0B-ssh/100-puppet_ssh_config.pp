bd6f8132230666ad5764dd6fed61aa288fb57c52
odogwukelly <odogwukellyice@gmail.com>
2023-07-04 03:59:20 -0700

    all task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,10 +3,12 @@ file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/sshd_config',
   line   => 'PasswordAuthentication no',
+  match  => '^#?\s*PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/sshd_config',
   line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?\s*IdentityFile',
 }
