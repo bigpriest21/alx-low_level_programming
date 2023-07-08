12915967f6effdf798aa0ea6cc2ddce126421195
odogwukelly <odogwukellyice@gmail.com>
2023-07-08 08:12:25 -0700

    all task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
 # Puppet script to create ssh config file
 file_line { 'Turn off passwd auth':
   ensure => 'present',
-  path   => '/etc/ssh/sshd_config',
-  line   => 'PasswordAuthentication no',
-  match  => '^#?\s*PasswordAuthentication',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => 'present',
-  path   => '/etc/ssh/sshd_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => '^#?\s*IdentityFile',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
