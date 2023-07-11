4df759089827b0204d8b92919699080ca3348eff
odogwukelly <odogwukellyice@gmail.com>
2023-07-11 02:28:33 -0700

    task 0

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,28 @@
-# Puppet script to create ssh config file
-file_line { 'Turn off passwd auth':
+class { 'stdlib': }
+
+file { '/home/ubuntu/.ssh/config':
   ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  mode   => '0600',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+  content => "
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
 
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
+file_line { 'turn_off_passwd_auth':
+  ensure => present,
+  path   => '/home/ubuntu/.ssh/config',
+  line   => 'PasswordAuthentication no',
+  match  => 'PasswordAuthentication',
 }
+
+file_line { 'declare_identity_file':
+  ensure => present,
+  path   => '/home/ubuntu/.ssh/config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => 'IdentityFile',
+}
+
