da075ef9d476f256011ed83bf4607770eaec544e
odogwukelly <odogwukellyice@gmail.com>
2023-07-11 04:58:20 -0700

    puppet ssh-config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,28 +1,15 @@
-class { 'stdlib': }
+#!/usr/bin/env bash
+# using puppet to configure ssh_config file
 
-file { '/home/ubuntu/.ssh/config':
-  ensure => 'present',
-  mode   => '0600',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  content => "
-    Host *
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-  ",
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content => "
+	
+	#ssh client or user configuration
+
+	Host *
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
 }
-
-file_line { 'turn_off_passwd_auth':
-  ensure => present,
-  path   => '/home/ubuntu/.ssh/config',
-  line   => 'PasswordAuthentication no',
-  match  => 'PasswordAuthentication',
-}
-
-file_line { 'declare_identity_file':
-  ensure => present,
-  path   => '/home/ubuntu/.ssh/config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => 'IdentityFile',
-}
-
