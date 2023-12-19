b41affae3dc7770f3e2bca408f8a02caf1f702c4
bigpriest21 <tizzydave4@gmail.com>
2023-12-18 21:43:09 -0800

    prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a c code that prints alphabet in lower case
+ * by big priest
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char lowerCase;
+
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+		putchar(lowerCase);
+
+	putchar('\n');
+
+	return (0);
+}
