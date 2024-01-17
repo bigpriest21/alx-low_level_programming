b3be371bf8f12f4d06aa6e918ff551363ed524d4
bigpriest21 <tizzydave4@gmail.com>
2024-01-16 20:51:02 -0800

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+#include <stdio.h>
+#include <unistd.h>
+
+void free_grid(int **grid, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);
+
+#endif
